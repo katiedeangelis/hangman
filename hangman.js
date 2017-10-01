57b4c69fd6df548588673bd46969e1e17b05fbcc
Katie DeAngelis <katie@Katies-MacBook-Pro.local>
2017-10-01 12:02:03 -0400

    Random movie selection and updated movie arrays into strings

--- hangman.js
+++ hangman.js
@@ -20,7 +20,18 @@
 // A movie is randomly choosen from the movie array
 var movieList = ["Citizen Kane", "The Godfather", "Casablanca", "Raging Bull", "Singin in the Rain", "Gone with the Wind", "Lawrence of Arabia", "Schindlers List", "Vertigo", "The Wizard of Oz", "City Lights", "The Searcher", "Star Wars", "Psycho", "Sunset Blvd", "The Graduate", "The General"]
 var movieChosen = movieList[Math.floor(Math.random() * movieList.length)];
+movieChosen = movieChosen.split("");
 console.log(movieChosen);
+var movieUnsolved = movieChosen.slice();
+
+for (var j = 0; j < movieUnsolved.length; j++) {
+    if (movieUnsolved[j] != " ") {
+        movieUnsolved[j] = "_";
+    } else {
+        movieUnsolved[j] = "<span class='title-spaces'></span>";
+    }
+}
+console.log(movieUnsolved);
 
 // User presses keys to guess letters
 var guessesLeft = 10;
@@ -28,9 +39,34 @@ var guessesLeft = 10;
 window.addEventListener("keyup", keyChosen);
 
 function keyChosen(e) {
-    var key = e.key;
-    guessesLeft = guessesLeft - 1;
+    var keyPressed = e.key;
+    keyPressed = keyPressed.toLowerCase();
     //guessesLeft -= 1 (shorthand)
+
+
+
+    //If the guesses left is less than one say sorry you lose
+    if (guessesLeft < 0) {
+        document.querySelector(".guesses-left").innerHTML = "Sorry, you lose!";
+    //Else show the number of guesses the user has left
+    } else {
+        document.querySelector(".guesses-left").innerHTML = guessesLeft;
+
+        //Now process current guess
+        for (var j = 0; j < movieChosen.length; j++) {
+            var currentChar = movieChosen[j];
+            currentChar = currentChar.toLowerCase();
+
+            //If key pressed guess is in the move print the letter to the console log
+            if (keyPressed == currentChar) {
+                console.log("Match! " + currentChar);
+                movieUnsolved[j] = movieChosen[j];
+                console.log(movieUnsolved);
+            }
+        }
+    }
+    document.querySelector(".movie").innerHTML = movieUnsolved.join(" ");
+    guessesLeft = guessesLeft - 1;
 }
 //6. If the letter is correct it is revealed
 //7. If the letter is incorrect the number of guesses integer decreases by one
