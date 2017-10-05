33562fc7b09d252193a8a35db023fea7abe43640
Katie DeAngelis <katie@Katies-MacBook-Pro.local>
2017-10-05 18:32:55 -0400

    CSS Responsiveness

--- hangman.js
+++ hangman.js
@@ -1,23 +1,4 @@
-//AFIs 100 Greatest American Movies
-
-//Use key events to listen for the letters that your players will type.
-//Display the following on the page:
-//Press any key to get started!
-//Wins: (# of times user guessed the word correctly).
-
-
-//If the word is madonna, display it like this when the game starts: _ _ _ _ _ _ _.
-//As the user guesses the correct letters, reveal them: m a d o _  _ a.
-
-
-//Number of Guesses Remaining: (# of guesses remaining for the user).
-//Letters Already Guessed: (Letters the user has guessed, displayed like L Z Y H).
-//After the user wins/loses the game should automatically choose another word and make the user play it.
-
-// Player opens hangman page
-// Screen reads "press any key to start"
-// Player presses a key, game starts
-// A movie is randomly choosen from the movie array
+// Alien movie array
 var movieList = [
     "Alien",
     "Avatar",
@@ -38,7 +19,7 @@ var movieList = [
     "Spaceballs",
     "Predator",
     "Contact",
-    "War or the Worlds",
+    "War of the Worlds",
     "The Day the Earth Stood Still",
     "Star Wars",
     "The Abyss",
